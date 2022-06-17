bf5fd97559a0561d5d2d7fb71cfd0d09e04815aa
Paul Shertzinger <pshertzi@gmail.com>
2022-06-17 16:41:13 -0400

    fixed link

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -31,7 +31,7 @@ const PhotoList = ({ category }) => {
       category: "Portfolio",
       description:
         "This food tracker app allows an individual to track the nutrition of the foods they eat, search for recipes involving the foods they're craving, and search for nearby locations of grocery stores with those items available.",
-      link: "https://access-tennesee.herokuapp.com/",
+      link: "https://pshertzi.github.io/Food-tracker-/",
       github: "https://github.com/pshertzi/Food-tracker-",
     },
     {
